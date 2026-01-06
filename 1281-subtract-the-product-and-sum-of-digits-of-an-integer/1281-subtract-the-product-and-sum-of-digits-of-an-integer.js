a617ea764c2262cede843794cf2c12d7c3fe5c98
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-06 12:16:10 +0530

    Time: 0 ms (100%), Space: 53.3 MB (86.31%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function (n) {
+    let product = 1, sum = 0
+    while (n !== 0) {
+        let rem = n % 10
+        sum += rem
+        product *= rem
+        n = Math.floor(n / 10)
+    }
+    return product - sum
+
+};
