74b80ab5d963686ac8a9324c3e85734ce2aabf61
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-16 10:51:08 +0530

    Time: 75 ms (22.18%), Space: 68.1 MB (27.44%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,33 @@
+var getIntersectionNode = function (headA, headB) {
+    let count1 = 0, count2 = 0
+    let p1 = headA, p2 = headB
+
+    while (p1) {
+        count1++
+        p1 = p1.next
+    }
+
+    while (p2) {
+        count2++
+        p2 = p2.next
+    }
+
+    p1 = headA
+    p2 = headB
+
+    let diff = Math.abs(count1 - count2)
+
+    if (count1 > count2) {
+        while (diff--) p1 = p1.next
+    } else {
+        while (diff--) p2 = p2.next
+    }
+
+    while (p1 && p2) {
+        if (p1 === p2) return p1
+        p1 = p1.next
+        p2 = p2.next
+    }
+
+    return null
+};
