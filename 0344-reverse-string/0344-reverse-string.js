e07994ead1fb89a9c9d2686085c5b7652464999b
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-18 15:39:32 +0530

    Time: 1 ms (57.16%), Space: 61.1 MB (58.72%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    let beg = 0, end = s.length - 1
+
+    while (beg <= end) {
+        let temp = s[beg]
+        s[beg] = s[end]
+        s[end] = temp
+
+        beg++
+        end--
+    }
+};
