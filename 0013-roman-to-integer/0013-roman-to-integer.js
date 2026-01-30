9ebd9460d9d2d71b58434afe5c497ba23b00b228
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-30 09:40:51 +0530

    Time: 10 ms (17.34%), Space: 61.9 MB (24.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -4,17 +4,32 @@
  */
 var romanToInt = function (s) {
     let map = new Map()
-    let symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    let values = [1, 5, 10, 50, 100, 500, 1000]
+    let symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M', 'IV', 'IX', 'XL', 'XC', 'CD', 'CM']
+    let values = [1, 5, 10, 50, 100, 500, 1000, 4, 9, 40, 90, 400, 900]
 
     for (let i = 0; i < symbols.length; i++) {
         map.set(symbols[i], values[i])
     }
+
+
+    let i = s.length - 1
     let sum = 0
-    for (let i of s) {
-        let x = map.get(i)
-        console.log(x)
-        sum += x
+    while (i >= 0) {
+        let val1 = map.get(s[i]) || 0
+        let val2 = map.get(s[i - 1]) || 0
+
+        if (val1 > val2) {
+            sum = sum + (val1 - val2)
+            i = i - 2
+        }
+        else {
+            sum = sum + val1
+            i--
+        }
     }
     return sum
+
+
+
+
 };
