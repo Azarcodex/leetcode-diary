a5d6962736843cc7cff572806145b6c5a7b4ee8b
Mohammed Azarin vp <azarcoder@gmail.com>
2026-02-06 10:08:03 +0530

    Time: 35 ms (18.95%), Space: 69.7 MB (11.66%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function (candyType) {
+    let map = new Map()
+    let max = candyType.length / 2
+    for (let i of candyType) {
+        if (map.has(i)) {
+            map.set(i, map.get(i) + 1)
+        }
+        else {
+            map.set(i, 1)
+        }
+    }
+
+    if (max >= map.size) {
+        return map.size
+    }
+    else {
+        return max
+    }
+};
+
+/*
+notes:
+n is also even
+alice is allowed only to eat n/2 and it should be maximum
+ */
