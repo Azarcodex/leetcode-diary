87637519243b759378393c0738127c7c86f762ea
Mohammed Azarin vp <azarcoder@gmail.com>
2026-02-07 12:02:05 +0530

    Time: 0 ms (100%), Space: 59.6 MB (31.85%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    function validate(root, min = -Infinity, max = Infinity) {
+        if (root === null) return true
+
+        if (root.val <= min || root.val >= max) {
+            return false
+        }
+        let leftTree = validate(root.left, min, root.val)
+        let rightTree = validate(root.right, root.val, max)
+        return leftTree && rightTree
+    }
+    return validate(root)
+};
