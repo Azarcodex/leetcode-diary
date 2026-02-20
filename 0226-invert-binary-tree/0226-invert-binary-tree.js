ecd112567badf7eaa5a527b40176cc3d1f2df223
Mohammed Azarin vp <azarcoder@gmail.com>
2026-02-20 07:56:49 +0530

    Time: 0 ms (100%), Space: 52.9 MB (96.36%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -13,9 +13,9 @@
 var invertTree = function (root) {
     if (root === null) return null
 
-    let temp=root.left
-    root.left=root.right
-    root.right=temp
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
 
     invertTree(root.left)
     invertTree(root.right)
